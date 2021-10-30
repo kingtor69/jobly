63ade499f166d0443c8c1fc1e869658c35cb85ef
Tor Kingdon <tor@hearkitty.com>
2021-10-29 22:47:00 -0600

    Part One done

--- helpers/sql.js
+++ helpers/sql.js
@@ -3,14 +3,26 @@ const { BadRequestError } = require("../expressError");
 // THIS NEEDS SOME GREAT DOCUMENTATION.
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  // this is used to convert JavaScript-friendly object keys into SQL-friendly column headers
+  // dataToUpdate represents the JS object of data to be converted to an SQL friendly string
+  // jsToSql is an object with comprised of the keys from dataToUpdate that need to be changed with the value on that key being a SQL-friendly string for the column header
+  
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
-
+  
+  // function returns an object containing two strings to be used in a db query
+    // setCols is the string of columns to be INSERTed into in sql format
+    // values are the values to be INSERT into the row
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
       `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
 
+  // '"first_name"=$1', '"age"=$2' => {
+  //   setCols: ['"first_name"=$1', '"age"=$2'],
+  //   values: [ 'Aliya', 33 ]
+  // }
+  // i think
   return {
     setCols: cols.join(", "),
     values: Object.values(dataToUpdate),
--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,18 +1,25 @@
 const { sqlForPartialUpdate } = require('./sql');
 
 const dataToUpdate = {
-    age: 33
+    firstName: 'Aliya',
+    age: '32'
 };
 
 const jsToSql = {
-    firstName: 'Aliya',
-    age: 32
+    firstName: "first_name"
 };
 
-const expectedSql = ['"first_name"=$1', '"age"=$2'];
+const expectedSql = {
+    "setCols": `"${jsToSql.firstName}"=$1, "age"=$2`,
+    "values": [ `${dataToUpdate.firstName}`, `${dataToUpdate.age}`]
+};
 
 describe("sqlForPartialUpdate", () => {
     test("non-garbage in, non-garbage out", () => {
         expect(sqlForPartialUpdate(dataToUpdate, jsToSql)).toEqual(expectedSql);
+    });
+    
+    test("garbage in, garbage out", () => {
+        expect(sqlForPartialUpdate(jsToSql, dataToUpdate)).not.toEqual(expectedSql);
     });
 });
